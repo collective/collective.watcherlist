e7d11d9a9be7295c19bfd724efe90d7583f55674
Maurits van Rees <maurits@vanrees.org>
2010-01-04 23:45:51 +0000

    No more IEmailSender interface needed
    svn path=/collective.watcherlist/trunk/; revision=107999

--- collective/watcherlist/interfaces.py
+++ collective/watcherlist/interfaces.py
@@ -7,17 +7,3 @@ class IWatcherList(Interface):
     watchers = Attribute("Members waning to get changes via email")
     extra_addresses = Attribute("Extra email addresses")
     send_mails = Attribute("Send mails yes/no")
-
-
-class IEmailSender(Interface):
-
-    def send_mail(view_name, **kw):
-        """Send e-mail.
-
-        Lookup browser view 'view_name' and use that to get the plain
-        text and or html and subject of the email.
-
-        Extra keyword arguments are passed to the update method of
-        that browser view.
-        """
-        pass
