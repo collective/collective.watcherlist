b7da7ec2199760189b52a63bf795a2be8128f06b
Maurits van Rees <maurits@vanrees.org>
2010-02-05 23:46:55 +0000

    We import from Plone, so we should depend on it in setup.py
    svn path=/collective.watcherlist/trunk/; revision=110389

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='collective.watcherlist',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'Plone',
       ],
       entry_points="""
       # -*- Entry points: -*-
