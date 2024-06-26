222e5c2c6e3c75b3abe7ccc6fd5128d85df89544
Maurits van Rees <maurits@vanrees.org>
2024-06-26 17:43:18 +0200

    Back to development: 4.0.0a2
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = open("README.rst").read()
 tests = open(os.path.join("collective", "watcherlist", "README.rst")).read()
 changes = open("CHANGES.rst").read()
 
-version = "4.0.0a1"
+version = "4.0.0a2.dev0"
 
 setup(
     name="collective.watcherlist",
